330c59ee192b4b0859e6e4129128d9f59721b4ed
JOSH <137239627+Joshikasri28@users.noreply.github.com>
2024-05-09 16:43:22 +0530

    cea.py

--- cea.py
+++ cea.py
@@ -1,10 +1,17 @@
-import streamlit as st
 import numpy as np
 import pandas as pd
 
+data = pd.DataFrame(data=pd.read_csv('tennis.csv'))
+concepts = np.array(data.iloc[:, 0:-1])
+target = np.array(data.iloc[:, -1])
+
 def learn(concepts, target):
     specific_h = concepts[0].copy()
+    print("Initialization of specific_h and general_h")
+    print(specific_h)
     general_h = [["?" for _ in range(len(specific_h))] for _ in range(len(specific_h))]
+    print(general_h)
+    
     for i, h in enumerate(concepts):
         if target[i] == "yes":
             for x in range(len(specific_h)):
@@ -17,24 +24,16 @@ def learn(concepts, target):
                     general_h[x][x] = specific_h[x]
                 else:
                     general_h[x][x] = '?'
+        print("Steps of Candidate Elimination Algorithm", i + 1)
+        print(specific_h)
+        print(general_h)
+    
     indices = [i for i, val in enumerate(general_h) if val == ['?', '?', '?', '?', '?', '?']]
     for i in indices:
         general_h.remove(['?', '?', '?', '?', '?', '?'])
+    
     return specific_h, general_h
 
-def run_algorithm(data_file):
-    data = pd.read_csv(data_file)
-    concepts = np.array(data.iloc[:, 0:-1])
-    target = np.array(data.iloc[:, -1])
-    s_final, g_final = learn(concepts, target)
-    return s_final, g_final
-
-st.title("Candidate Elimination Algorithm")
-
-uploaded_file = st.file_uploader("Upload CSV file", type="csv")
-if uploaded_file is not None:
-    s_final, g_final = run_algorithm(uploaded_file)
-    st.subheader("Final Specific_h:")
-    st.write(s_final)
-    st.subheader("Final General_h:")
-    st.write(g_final)
+s_final, g_final = learn(concepts, target)
+print("Final Specific_h:", s_final, sep="\n")
+print("Final General_h:", g_final, sep="\n")
