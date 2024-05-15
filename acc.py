2f391351a43dc36d8302c1543c8f8aec8b504fd4
JOSH <137239627+Joshikasri28@users.noreply.github.com>
2024-05-15 14:21:30 +0530

    acc.py

--- acc.py
+++ acc.py
@@ -1,8 +1,3 @@
-!pip install scikit-learn
-!pip show scikit-learn
-!pip install --upgrade scikit-learn
-!pip install --upgrade scikit-learn
-
 import streamlit as st
 import pandas as pd
 from sklearn.naive_bayes import GaussianNB
